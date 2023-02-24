a4e1804d7e5bf73ba400f805a3af766eb7487ac6
EskalarteDexter <88998149+EskalarteDexter@users.noreply.github.com>
2023-02-24 18:39:43 +0800

    Update and rename mtk-vless-vmess-ws-tls.sh to mtk-vless-ws-tls.sh

--- mtk-vless-ws-tls.sh
+++ mtk-vless-ws-tls.sh
@@ -7,7 +7,6 @@ echo "
 ██║  ██║██╔══╝   ██╔██╗    ██║   ██╔══╝  ██╔══██╗
 ██████╔╝███████╗██╔╝ ██╗   ██║   ███████╗██║  ██║  
 "
-echo
 echo "=== V2ray MTK with Caddy [Vless/Vmess+WS+TLS] (Ubuntu 22.04) ==="
 echo
 sleep 3
